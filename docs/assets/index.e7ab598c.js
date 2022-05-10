d85a259d120b2a4c56ae4130372f65e28df0b938
Cheng_DX <2337040896@qq.com>
2022-05-10 23:29:58 +0800

    build: update built production

---
+++ docs/assets/index.e7ab598c.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ExceptionBase.c6773ee9.js";import{d as o,c as r,u as t,o as a}from"./index.cb1350d2.js";const f=o({setup(s){return(c,n)=>(a(),r(t(e),{type:"403"}))}});export{f as default};
