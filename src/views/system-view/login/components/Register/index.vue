1bdd4cef24ebf5cbaeb65abf9e48220f4514d877
Cheng_DX <2337040896@qq.com>
2022-05-10 20:56:40 +0800

    feat: add role choice radio in register panel

--- src/views/system-view/login/components/Register/index.vue
+++ src/views/system-view/login/components/Register/index.vue
@@ -3,6 +3,14 @@
     <n-form-item path="username">
       <n-input v-model:value="model.username" placeholder="请输入用户名" />
     </n-form-item>
+    <n-form-item path="role">
+      <n-radio-group v-model:value="model.role" placeholder="请选择角色" style="width: 100%">
+        <n-space justify="center">
+          <n-radio :value="EnumUserRole.admin">云工厂管理员</n-radio>
+          <n-radio :value="EnumUserRole.dealer">经销商</n-radio>
+        </n-space>
+      </n-radio-group>
+    </n-form-item>
     <n-form-item path="phone">
       <n-input v-model:value="model.phone" placeholder="手机号码" />
     </n-form-item>
@@ -35,6 +43,7 @@ import type { FormInst, FormRules } from 'naive-ui';
 import { useRouterPush } from '@/composables';
 import { useSmsCode } from '@/hooks';
 import { formRules, getConfirmPwdRule } from '@/utils';
+import { EnumUserRole } from '@/enum/business';
 
 const { toLoginModule } = useRouterPush();
 const { label, isCounting, loading: smsLoading, start } = useSmsCode();
@@ -43,6 +52,7 @@ const formRef = ref<(HTMLElement & FormInst) | null>(null);
 const model = reactive({
   username: '',
   phone: '',
+  role: EnumUserRole.admin,
   code: '',
   pwd: '',
   confirmPwd: ''
@@ -71,7 +81,7 @@ function handleSubmit(e: MouseEvent) {
         window.$message?.error('请先阅读并同意《用户协议》《隐私权政策》');
         return;
       }
-      window.$message?.success('验证成功!');
+      window.$message?.success('注册成功!');
     } else {
       window.$message?.error('验证失败!');
     }
