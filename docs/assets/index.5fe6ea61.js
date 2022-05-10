ca2cc1203a5b486c6b2e82ac908a824bcc9955c3
Cheng_DX <2337040896@qq.com>
2022-05-10 23:54:01 +0800

    delete

--- docs/assets/index.5fe6ea61.js
+++
@@ -1,1 +0,0 @@
-import{d as e,o,c as a,u as s}from"./index.2c320f72.js";import{b as r}from"./index.616a9ea6.js";const _=e({setup(n){return(t,c)=>(o(),a(s(r),{"show-padding":!1}))}});export{_ as default};
--- docs/assets/useBoolean.97a0ae87.js
+++
@@ -1,1 +0,0 @@
-import{r as l}from"./index.2c320f72.js";function a(t=!1){const o=l(t);function e(f){o.value=f}function n(){e(!0)}function u(){e(!1)}function s(){e(!o.value)}return{bool:o,setBool:e,setTrue:n,setFalse:u,toggle:s}}export{a as u};
