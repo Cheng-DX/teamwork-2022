54384959a65be83cac8ab034ba69604f5b8aa11f
Cheng_DX <2337040896@qq.com>
2022-05-11 23:21:36 +0800

    feat: add routes in mock

--- mock/model/route.ts
+++ mock/model/route.ts
@@ -10,24 +10,90 @@ export const routeModel: Record<Auth.RoleType, AuthRoute.Route[]> = {
           path: '/dashboard/analysis',
           component: 'self',
           meta: {
-            title: '分析页',
+            title: '流量分析',
+            requiresAuth: true,
+            icon: 'icon-park-outline:analysis'
+          }
+        }
+      ],
+      meta: {
+        title: '数据总览',
+        icon: 'carbon:dashboard',
+        order: 1
+      }
+    },
+    {
+      name: 'user-management',
+      path: '/user-management',
+      component: 'self',
+      meta: {
+        title: '用户管理',
+        requiresAuth: true,
+        singleLayout: 'basic',
+        icon: 'carbon:dashboard',
+        order: 1
+      }
+    },
+    {
+      name: 'product-management',
+      path: '/product-management',
+      component: 'basic',
+      children: [
+        {
+          name: 'product-management_product',
+          path: '/product-management/product',
+          component: 'self',
+          meta: {
+            title: '产品',
             requiresAuth: true,
             icon: 'icon-park-outline:analysis'
           }
         },
         {
-          name: 'dashboard_workbench',
-          path: '/dashboard/workbench',
+          name: 'product-management_product-type',
+          path: '/product-management/product-type',
           component: 'self',
           meta: {
-            title: '工作台',
+            title: '产品类别',
             requiresAuth: true,
-            icon: 'icon-park-outline:workbench'
+            icon: 'icon-park-outline:analysis'
           }
         }
       ],
       meta: {
-        title: '仪表盘',
+        title: '产品管理',
+        icon: 'carbon:dashboard',
+        order: 1
+      }
+    },
+    {
+      name: 'equipment-management',
+      path: '/equipment-management',
+      component: 'basic',
+      children: [
+        {
+          name: 'equipment-management_equipment',
+          path: '/equipment-management/equipment',
+          component: 'self',
+          meta: {
+            title: '设备',
+            requiresAuth: true,
+            icon: 'icon-park-outline:analysis'
+          }
+        },
+        {
+          name: 'equipment-management_equipment-type',
+          path: '/equipment-management/equipment-type',
+          component: 'self',
+          meta: {
+            title: '设备类别',
+            requiresAuth: true,
+            icon: 'icon-park-outline:analysis'
+          }
+        }
+      ],
+      meta: {
+        title: '设备管理',
         icon: 'carbon:dashboard',
         order: 1
       }
@@ -121,28 +187,5 @@ export const routeModel: Record<Auth.RoleType, AuthRoute.Route[]> = {
       }
     }
   ],
-  user: [
-    {
-      name: 'dashboard',
-      path: '/dashboard',
-      component: 'basic',
-      children: [
-        {
-          name: 'dashboard_analysis',
-          path: '/dashboard/analysis',
-          component: 'self',
-          meta: {
-            title: '分析页',
-            requiresAuth: true,
-            icon: 'icon-park-outline:analysis'
-          }
-        }
-      ],
-      meta: {
-        title: '仪表盘',
-        icon: 'carbon:dashboard',
-        order: 1
-      }
-    }
-  ]
+  dealer: []
 };
