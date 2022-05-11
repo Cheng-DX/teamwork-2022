99b252f0a46fb8fdec86484cf00e019e158b47e7
Cheng_DX <2337040896@qq.com>
2022-05-11 23:51:50 +0800

    feat: equipment

---
+++ src/data/superAdmin/equipment/index.tsx
@@ -0,0 +1,128 @@
+import { Ref, ref } from 'vue';
+import { useTable } from '@/data/utils/useTable'
+import { type InternalRowData } from 'naive-ui/lib/data-table/src/interface';
+import { type ColumnSrcItem } from '@/data/utils/creator';
+import { NButton, NTag } from 'naive-ui';
+
+export enum EquipmentStatus {
+  /** 闲置 */
+  Opened = '闲置中',
+  /** 关机 */
+  Closed = '已关机',
+  /** 故障 */
+  Fault = '故障中',
+  /** 生产中 */
+  Producing = '生产中',
+}
+
+export enum rentStatus {
+  /** 租用设备 */
+  Rent = '租用设备',
+  /** 自有设备 */
+  Own = '自有设备',
+}
+
+function getType(status: string) {
+  switch (status) {
+    case EquipmentStatus.Opened:
+      return 'success';
+    case EquipmentStatus.Closed:
+      return 'warning';
+    case EquipmentStatus.Fault:
+      return 'error';
+    case EquipmentStatus.Producing:
+      return 'info';
+    default:
+      return 'info';
+  }
+}
+
+function createColumns(data: Ref<any[]>) {
+  const columnSrc: ColumnSrcItem[] = [
+    {
+      title: '序号',
+      key: 'index',
+    },
+    {
+      title: '设备编号',
+      key: 'id',
+      renderer: (row: InternalRowData) => {
+        return (<span>{row.id}</span>)
+      }
+    },
+    {
+      title: '设备名称',
+      key: 'name',
+    },
+    {
+      title: '设备类别',
+      key: 'type',
+      renderer: (row: InternalRowData) => {
+        return (<span >{row.type}</span>)
+      }
+    },
+    {
+      title: '设备规格',
+      key: 'spec',
+    },
+    {
+      title: '设备描述',
+      key: 'description',
+    },
+    {
+      title: '设备状态',
+      key: 'status',
+      renderer: (row: InternalRowData) => {
+        return (<NTag strong type={getType(row.status as string)} >{row.status}</NTag>)
+      }
+    },
+    {
+      title: '所属工厂',
+      key: 'factory',
+    },
+    {
+      title: '租用状态',
+      key: 'rentStatus',
+    },
+    {
+      title: '开关机',
+      key: 'power',
+      renderer: (row: InternalRowData) => {
+        return (<NButton strong secondary type={row.status === EquipmentStatus.Closed ? 'success' : 'error'} onClick={() => switchStatus(row.id as string, data)}>{row.status === EquipmentStatus.Closed ? '远程开机' : '远程关机'}</NButton>)
+      }
+    }
+  ]
+  return columnSrc
+}
+
+function switchStatus(id: string, data: Ref<any[]>) {
+  const target = data.value.find((item) => item.id === id)
+  if (target) {
+    target.status = target.status === EquipmentStatus.Closed ? EquipmentStatus.Opened : EquipmentStatus.Closed
+  }
+}
+
+function createEquipments() {
+  const data = [];
+  const time = new Date().getTime()
+  for (let i = 0; i < 40; i++) {
+    data.push({
+      index: i + 1,
+      id: `DNO${time + i * 2387478}`,
+      name: `设备${i}`,
+      type: `类别${i}`,
+      spec: `规格${i}`,
+      description: `描述${i}`,
+      status: EquipmentStatus.Opened,
+      factory: `云工厂${i}`,
+      rentStatus: rentStatus.Own,
+    })
+  }
+  return data
+}
+
+export function useEquipments() {
+  const data = ref(createEquipments())
+  const columns = useTable(data, createColumns(data), 'id')
+  return { data, columns }
+}
--- src/views/equipment-management/equipment/index.vue
+++ src/views/equipment-management/equipment/index.vue
@@ -1,5 +1,47 @@
-<script setup lang="ts"></script>
+<template>
+  <n-space :vertical="true" :size="16">
+    <n-space vertical>
+      <quick-table title="设备" :columns="columns" :data="dispaly" :page-size="10" :height="480">
+        <n-space justify="space-between">
+          <n-button type="primary" secondary strong @click="handleAdd">添加设备</n-button>
+          <n-input v-model:value="search" placeholder="搜索" round clearable>
+            <template #prefix>
+              <n-icon :component="FlashOutline" />
+            </template>
+          </n-input>
+        </n-space>
+      </quick-table>
+    </n-space>
+  </n-space>
+</template>
 
-<template>设备</template>
+<script lang="ts" setup>
+import { computed, DefineComponent, h, ref } from 'vue';
+import { useDialog } from 'naive-ui';
+import { FlashOutline } from '@vicons/ionicons5';
+import { Register } from '@/views/system-view/login/components';
+import QuickTable from '@/components/quickTable/index.vue';
+import { useEquipments } from '@/data/superAdmin/equipment/index';
 
+const { data, columns } = useEquipments();
+const dialog = useDialog();
+
+const search = ref('');
+const dispaly = computed(() => {
+  if (search.value) {
+    return data.value.filter(item => item.name.includes(search.value));
+  }
+  return data.value;
+});
+
+function handleAdd() {
+  dialog.info({
+    title: '添加用户',
+    content: () =>
+      h(Register as DefineComponent, {
+        disableBack: true
+      })
+  });
+}
+</script>
 <style scoped></style>
