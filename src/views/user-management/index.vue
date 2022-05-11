c3290beea8719481eeba7cb7f367cb167f109ee5
Cheng_DX <2337040896@qq.com>
2022-05-11 23:20:53 +0800

    feat: user

---
+++ src/views/user-management/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <n-space :vertical="true" :size="16">
+    <n-space vertical>
+      <quick-table title="用户管理" :columns="columns" :data="dispaly" :page-size="10" :height="480">
+        <n-space justify="space-between">
+          <n-button type="primary" secondary strong @click="handleAdd">添加用户</n-button>
+          <n-input v-model:value="search" placeholder="搜索" round clearable>
+            <template #prefix>
+              <n-icon :component="FlashOutline" />
+            </template>
+          </n-input>
+        </n-space>
+      </quick-table>
+    </n-space>
+  </n-space>
+</template>
+
+<script lang="ts" setup>
+import { computed, DefineComponent, h, ref } from 'vue';
+import { useDialog } from 'naive-ui';
+import { FlashOutline } from '@vicons/ionicons5';
+import { useUsers } from '@/data/superAdmin/user';
+import { Register } from '@/views/system-view/login/components';
+import QuickTable from '@/components/quickTable/index.vue';
+
+const { data, columns } = useUsers();
+const dialog = useDialog();
+
+const search = ref('');
+const dispaly = computed(() => {
+  if (search.value) {
+    return data.value.filter(item => item.username.includes(search.value));
+  }
+  return data.value;
+});
+
+function handleAdd() {
+  dialog.info({
+    title: '添加用户',
+    content: () =>
+      h(Register as DefineComponent, {
+        disableBack: true
+      })
+  });
+}
+</script>
+<style scoped></style>
