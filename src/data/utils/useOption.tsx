73afa0f6eff11c84d8d6d53409a57fa227e272ef
Cheng_DX <2337040896@qq.com>
2022-05-12 15:48:58 +0800

    feat: rewrite delete option by useOption

---
+++ src/data/utils/useOption.tsx
@@ -0,0 +1,46 @@
+import { DataTableColumn, NButton } from "naive-ui"
+import { InternalRowData } from "naive-ui/lib/data-table/src/interface"
+
+export interface Filter {
+  handler: (row: InternalRowData) => boolean
+  rejectAction?: Function
+  acceptAction?: Function
+  returnImdiately?: boolean
+}
+
+type DeleteFn = (row: InternalRowData) => void
+
+export function useDelete(deleteFn: DeleteFn, filter?: Filter): DataTableColumn {
+  function handle(row: InternalRowData) {
+    if (!filter?.handler(row)) {
+      if (filter?.rejectAction) {
+        filter.rejectAction()
+      }
+      if (filter?.returnImdiately) {
+        return
+      }
+    } else {
+      if (filter?.acceptAction) {
+        filter.acceptAction()
+      }
+    }
+    window.$dialog?.warning({
+      title: 'Warning',
+      content: `确定要删除吗?`,
+      positiveText: '确定',
+      onPositiveClick() {
+        deleteFn(row)
+      },
+      negativeText: '取消',
+    })
+  }
+
+  return {
+    title: '删除',
+    key: 'delete',
+    align: 'center',
+    render: (row: InternalRowData) => (<div class="flex justify-evenly" >
+      <NButton secondary strong onClick={() => handle(row)} type="error">删除</NButton>
+    </div>)
+  }
+}
--- src/data/utils/useTable.tsx
+++ src/data/utils/useTable.tsx
@@ -1,53 +1,7 @@
-import { NButton } from 'naive-ui'
-import { InternalRowData } from 'naive-ui/lib/data-table/src/interface'
 import { ref, type Ref } from 'vue'
 import { type ColumnSrcItem, createColumns } from './creator'
 
-export interface Filter {
-  handler: (row: InternalRowData) => boolean
-  rejectAction?: Function
-  acceptAction?: Function
-  returnImdiately?: boolean
-}
-
-export function useTable(data: Ref<any[]>, columnSrc: ColumnSrcItem[], deleteBy: string, filter?: Filter) {
-  if (!columnSrc.find(item => item.key === 'delete')) {
-    columnSrc.push(
-      {
-        title: '删除',
-        key: 'delete',
-        renderer: (row: InternalRowData) => (<div class="flex justify-evenly" >
-          <NButton secondary strong onClick={() => deleteOne(row, data)} type="error">删除</NButton>
-        </div>)
-      }
-    )
-  }
-
-
-  function deleteOne(row: InternalRowData, data: Ref<any[]>) {
-    if (!filter?.handler(row)) {
-      if (filter?.rejectAction) {
-        filter.rejectAction()
-      }
-      if (filter?.returnImdiately) {
-        return
-      }
-    } else {
-      if (filter?.acceptAction) {
-        filter.acceptAction()
-      }
-    }
-    window.$dialog?.warning({
-      title: 'Warning',
-      content: `确定要删除吗?`,
-      positiveText: '确定',
-      onPositiveClick() {
-        data.value = data.value.filter(item => item[deleteBy] !== row[deleteBy])
-      },
-      negativeText: '取消',
-    })
-  }
-
+export function useTable(data: Ref<any[]>, columnSrc: ColumnSrcItem[]) {
   const columns = createColumns(columnSrc, data)
   return ref(columns)
 }
