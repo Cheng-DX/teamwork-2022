1675987063ef8d1bd9bd5da3c1c5fea8da3ceec8
Cheng_DX <2337040896@qq.com>
2022-05-16 20:01:42 +0800

    new

---
+++ docs/assets/index.7ede91bb.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ExceptionBase.f97d7481.js";import{d as o,e as r,g as t,o as a}from"./index.296a0779.js";const f=o({setup(s){return(n,c)=>(a(),r(t(e),{type:"500"}))}});export{f as default};
--- docs/assets/index.fb1262f9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./ExceptionBase.90d2ff51.js";import{d as o,e as r,g as t,o as a}from"./index.eb814750.js";const f=o({setup(s){return(n,c)=>(a(),r(t(e),{type:"403"}))}});export{f as default};
